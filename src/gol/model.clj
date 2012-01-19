3220139c7c8f340784ea582cd221862a95de1de7
Sebastian Ortega <sortega@tid.es>
2012-01-19 20:53:32 +0100

    Game of life model

---
+++ src/gol/model.clj
@@ -0,0 +1,21 @@
+(ns gol.model)
+
+(defn neighbours [cell]
+  (map
+    (fn [delta] (map + cell delta)) [[-1 -1] [-1 0] [-1 1]
+                                     [ 0 -1]        [ 0 1]
+                                     [ 1 -1] [ 1 0] [ 1 1]]))
+(defn survive? [neigs]
+  (and (>= neigs 2) (<= neigs 3)))
+
+(defn newborn? [neigs]
+  (== neigs 3))
+
+(defn next-gen [cells]
+  (let [neigs     (frequencies (mapcat neighbours cells))
+        survivors (filter (fn [cell] (survive? (get neigs cell 0))) cells)
+        newborns  (map first (filter #(newborn? (second %)) neigs))]
+       (set (concat survivors newborns))))
+
+(defn gen-seq [world]
+  (iterate next-gen world))
---
+++ test/gol/test/model.clj
@@ -0,0 +1,18 @@
+(ns gol.test.model
+  (:use [gol.model])
+  (:use [clojure.test]))
+
+(deftest neighbours-test
+  (is (= [[ 9 9] [ 9 10] [ 9 11]
+          [10 9]         [10 11]
+          [11 9] [11 10] [11 11]]
+        (neighbours [10 10]))))
+
+(deftest next-gen-test
+  (is (= #{} (next-gen #{})))
+  (is (= #{} (next-gen #{[0 0]})))
+  (is (= #{[0 0] [1 0] [-1 0]} (next-gen #{[0 0] [0 1] [0 -1]}))))
+
+(deftest gen-seq-test
+  (is (= #{[0 0] [1 0] [-1 0]}
+        (nth (gen-seq #{[0 0] [0 1] [0 -1]}) 201))))
